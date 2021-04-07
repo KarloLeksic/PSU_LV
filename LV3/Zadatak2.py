9bcecf80fed0c87719bf85b5115acf056690e070
KarloLeksic <lekxka@gmail.com>
2021-04-07 09:23:39 +0200

    LV3

--- LV3/Zadatak2.py
+++ LV3/Zadatak2.py
@@ -8,9 +8,13 @@ mt4 = mtcars[(mtcars.cyl == 4)].mean()
 mt6 = mtcars[(mtcars.cyl == 6)].mean()
 mt8 = mtcars[(mtcars.cyl == 8)].mean()
 
-label = ["4 cilindra", "6 cilindra", "8cilindra"]
-values = [mt4.mpg, mt6.mpg, mt8.mpg]
+label_cyl = ["4 cilindra", "6 cilindra", "8cilindra"]
+values_mpg = [mt4.mpg, mt6.mpg, mt8.mpg]
 
-plt.bar(label, values)
+
+plt.bar(label_cyl, values_mpg)
 plt.ylabel("MPG")
+plt.show()
+
+boxplot = mtcars.boxplot()
 plt.show()
